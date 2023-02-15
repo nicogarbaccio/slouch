ac78a797a48d816e520ff433a388dc3ddc93e44f
Nico Garbaccio <garbaccio20@gmail.com>
2023-02-15 15:27:52 -0500

    Initialize React app, install Firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBwlbJTYDK0fhD1fdp1YASf6ZE8NgMctKw",
+    authDomain: "slouch-dd087.firebaseapp.com",
+    projectId: "slouch-dd087",
+    storageBucket: "slouch-dd087.appspot.com",
+    messagingSenderId: "434718386256",
+    appId: "1:434718386256:web:b6bc3799493eb24c07c955",
+    measurementId: "G-YEHG022CE8"
+  };
