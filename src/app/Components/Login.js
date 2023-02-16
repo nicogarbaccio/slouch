f981fd8f890f9a3f76f6d52102e1165655012903
Nico Garbaccio <garbaccio20@gmail.com>
2023-02-16 14:33:05 -0500

    Implement Google auth

--- src/app/Components/Login.js
+++ src/app/Components/Login.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
+import Button from '@mui/material/Button';
+import { auth, provider } from '../../firebase';
+import { signInWithPopup } from 'firebase/auth';
 
 function Login() {
+    const signIn = e => {
+        e.preventDefault();
+        signInWithPopup(auth, provider).catch((error) => alert(error.message));
+    }
+
   return (
     <LoginContainer className=''>
         <LoginInnerContainer>
             <img src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg" alt=""/>
+            <h1>Sign into Slouch</h1>
+            <p>hq.slouch.com</p>
+            <Button onClick={signIn}>Sign in with Google</Button>
         </LoginInnerContainer>
     </LoginContainer>
   )
@@ -14,13 +25,27 @@ function Login() {
 export default Login
 
 const LoginContainer = styled.div`
-
+    background-color: #f8f8f8;
+    height: 100vh;
+    display: grid;
+    place-items: center;
 `;
 
 const LoginInnerContainer = styled.div`
+    padding: 100px;
+    text-align: center;
+    background-color: white;
+    border-radius: 10px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
     > img {
         object-fit: contain;
         height: 100px;
         margin-bottom: 40px;
+    }
+    > button {
+        margin-top: 50px;
+        text-transform: inherit !important;
+        background-color: #0a8d48 !important;
+        color: white;
     }
 `;
