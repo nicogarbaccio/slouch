899be5999f16396f64e98d1db491dedf02aea49e
Nico Garbaccio <garbaccio20@gmail.com>
2023-02-17 22:13:28 -0500

    Reconfigure firebase

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,15 @@ import { GoogleAuthProvider } from "firebase/auth";
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBwlbJTYDK0fhD1fdp1YASf6ZE8NgMctKw",
-    authDomain: "slouch-dd087.firebaseapp.com",
-    projectId: "slouch-dd087",
-    storageBucket: "slouch-dd087.appspot.com",
-    messagingSenderId: "434718386256",
-    appId: "1:434718386256:web:b6bc3799493eb24c07c955",
-    measurementId: "G-YEHG022CE8"
-  };
+  apiKey: "AIzaSyBwlbJTYDK0fhD1fdp1YASf6ZE8NgMctKw",
+  authDomain: "slouch-dd087.firebaseapp.com",
+  databaseURL: "https://slouch-dd087-default-rtdb.firebaseio.com",
+  projectId: "slouch-dd087",
+  storageBucket: "slouch-dd087.appspot.com",
+  messagingSenderId: "434718386256",
+  appId: "1:434718386256:web:6972c3d9db5935b007c955",
+  measurementId: "G-DJXRE31T10"
+};
 
   const app = initializeApp(firebaseConfig);
 
